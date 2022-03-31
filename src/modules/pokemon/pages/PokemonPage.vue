62fae083c466cb94a0ae3c6c52484e8d2cd75224
German <german.rodriguezgu@gmail.com>
2022-03-30 22:30:52 -0500

    :hammer: :construction: Id params

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,3 +1,22 @@
 <template>
     <h1>Pokemon page</h1>
+    <span>{{id}}</span>
 </template>
+
+<script>
+export default {
+    props:{
+        id:{
+            type:Number,
+            required:true,
+        }
+    },
+    data(){
+        return{
+        }
+    },
+    created() {
+    //    const {id} = this.$route.params
+    }
+}
+</script>
