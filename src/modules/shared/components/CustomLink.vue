9177ec87728cb10462de250175924426361c2e88
German <german.rodriguezgu@gmail.com>
2022-04-03 20:24:28 -0500

    :sparkles: :lipstick: Custom link para el navbar

---
+++ src/modules/shared/components/CustomLink.vue
@@ -0,0 +1,43 @@
+<template>
+  <a
+    v-if="isExternalLink"
+    target="_blank"
+    :href="link.to"
+    class="normal-link"
+    >
+    {{ link.name }}
+    </a>
+  <router-link v-else :to="link.to" v-slot="{ href, isActive }"
+    ><a :class="isActive ? 'is-active' : 'normal-link'" :href="href">{{
+      link.name
+    }}</a></router-link
+  >
+</template>
+
+
+
+<script>
+export default {
+  props: {
+    link: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    isExternalLink() {
+      return this.link.to.startsWith("https");
+    },
+  },
+};
+</script>
+
+<style scope>
+.is-active {
+  color: #11cf00;
+}
+
+.normal-link {
+  color: #c6c5c5;
+}
+</style>
