a9e6b9cd4ed1c1ec438ebba64be2b9cca7ee7de8
German <german.rodriguezgu@gmail.com>
2022-04-03 20:24:54 -0500

    :zap: arreglo de los links

--- src/modules/shared/components/NavBar.vue
+++ src/modules/shared/components/NavBar.vue
@@ -1,14 +1,33 @@
+
 <template>
   <div>
-    <router-link to="/">Pokemos List</router-link>
-    <router-link to="/id">Pokemos por id</router-link>
-
-    <router-link to="/about">About</router-link>
+    <!-- <router-link :to="{ name: 'home' }">Pokemos List</router-link>
+    <router-link :to="{ name: 'pokemon-id', params: { id: 85 } }"
+      >Pokemos por id</router-link
+    >
+    <router-link :to="{ name: 'about' }">About</router-link> -->
+    <CustomLink v-for="link in links" :key="link.to" :link="link"/>
   </div>
 </template>
 
 <script>
-export default {};
+import { defineAsyncComponent } from "vue";
+
+export default {
+  data() {
+    return {
+      links: [
+        { to: "/home", name: "Pokemons" },
+        { to: "/pokemonid/50", name: "Por ID" },
+        { to: "/about", name: "About" },
+        { to: "https://google.com", name: "Google" },
+      ],
+    };
+  },
+  components: {
+    CustomLink: defineAsyncComponent(() => import("./CustomLink.vue")),
+  },
+};
 </script>
 
 <style scoped>
@@ -17,10 +36,9 @@ div {
 }
 div a {
   font-weight: bold;
-  color: #004606;
   margin: 0 10px;
 }
-a.router-link-exact-active{
-    color: #11cf00;
-}
+/* a.router-link-exact-active {
+  color: #11cf00;
+} */
 </style>
