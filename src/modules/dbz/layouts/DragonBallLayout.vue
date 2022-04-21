f18eec41c0fbbbadc5b821537712ad2c45fa7f0a
German <german.rodriguezgu@gmail.com>
2022-04-20 22:16:37 -0500

    :zap: :lipstick: Layout component para manejo de rutas hijas y demas componentes

---
+++ src/modules/dbz/layouts/DragonBallLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="dragon-ball-layout">
+    <h1>DBZ Layout</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+    .dragon-ball-layout {
+        background-color: #ff9e9e;
+    }
+</style>
